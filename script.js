5f80add27f22688027c1d15f6b1581d540c90a63
Mikee Ojastro <mikeeojastro@gmail.com>
2025-03-19 19:05:34 +0800

    March 19, 2025

---
+++ script.js
@@ -0,0 +1,42 @@
+let board = document.getElementById("board");
+let prev = document.getElementById("prev");
+
+let gameBoard = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+
+let state = [];
+
+let moves = 0;
+
+let playerTurn1 = true;
+
+function createBoard(){
+
+}
+
+function addMove(element, boxNumber){
+
+}
+
+function updateBoard(element, boxNumber){
+
+}
+
+function updateState(boardCopy){
+
+}
+
+function checkEndGame(){
+
+}
+
+function reflectBoard(index){
+    
+}
+
+prev.addEventListener("click", () => reflectBoard(7));
+
+createBoard();
